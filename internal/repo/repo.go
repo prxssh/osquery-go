2f39b7c89ecf8e77b476d1dbade68de186e507fc
prxssh <prxssh@pm.me>
2025-04-05 22:09:24 +0530

    refactor: remove unwanted function

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,9 +1,6 @@
 package repo
 
 import (
-	"context"
-	"database/sql"
-
 	"github.com/prxssh/osquery-go/config/postgres"
 )
 
@@ -21,30 +18,4 @@ func NewRepo(dbClient *postgres.PostgresClient) *Repo {
 		OsVersion:   NewOsVersionRepo(dbClient),
 		OsqueryInfo: NewOsqueryInfoRepo(dbClient),
 	}
-}
-
-func (r *Repo) BeginTx() (*sql.Tx, error) {
-	return r.client.Begin()
-}
-
-func (r *Repo) ExecuteTransaction(
-	ctx context.Context,
-	operation func(txn *sql.Tx) (any, error),
-) (any, error) {
-	txn, err := r.client.Begin()
-	if err != nil {
-		return nil, err
-	}
-	defer txn.Rollback()
-
-	res, err := operation(txn)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := txn.Commit(); err != nil {
-		return nil, err
-	}
-
-	return res, nil
 }
